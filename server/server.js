c428c43d60732e889f10aadfea6ecc7a2be7e403
Siraj Mohammed <bstech500@gmail.com>
2025-07-02 00:53:30 +0300

    inintial commit

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ import postRouter from "./routes/blog.route.js";
 dotenv.config();
 const app = express();
 app.use(cors({
-    origin: "http://localhost:5175",
+    origin: "http://localhost:5175, https://user-post-vd57.onrender.com/",
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH']
 }));
